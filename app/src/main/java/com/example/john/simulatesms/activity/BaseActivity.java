3d22fe5c34b0d0cfdaf42d7c5f36131e57634ac3
john <liluping789@gmail.com>
2016-11-19 22:45:43 +0800

    创建BaseActivity

---
+++ app/src/main/java/com/example/john/simulatesms/activity/BaseActivity.java
@@ -0,0 +1,51 @@
+package com.example.john.simulatesms.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+        initData();
+        initListener();
+    }
+
+    /**
+     * 初始话视图
+     */
+    public abstract void initView();
+
+
+    /**
+     * 初始化数据
+     */
+    public abstract void initData();
+
+
+    /**
+     * 初始化监听
+     */
+    public abstract void initListener();
+
+    /**
+     * 处理点击事件
+     *
+     * @param view 事件源（控件）
+     */
+    public abstract void handleClickEvent(View view);
+
+
+    /**
+     * 点击事件处理
+     *
+     * @param view 控件
+     */
+    @Override
+    public void onClick(View view) {
+        handleClickEvent(view);
+    }
+}
---
+++ app/src/main/java/com/example/john/simulatesms/activity/SMSActivity.java
@@ -0,0 +1,33 @@
+package com.example.john.simulatesms.activity;
+
+import android.view.View;
+
+import com.example.john.simulatesms.R;
+
+/**
+ * Created by John on 2016/11/19.
+ * <p>
+ * 消息显示活动
+ */
+
+public class SMSActivity extends BaseActivity {
+    @Override
+    public void initView() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void handleClickEvent(View view) {
+
+    }
+}
